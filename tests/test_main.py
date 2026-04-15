17693dbd6e691a197413ffd2ebf7d5fcf6ab0fac
rafaeldesouza999 <rafaeldesouza1678@gmail.com>
2026-04-14 21:02:39 -0300

    codigo base

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,25 +9,25 @@ client = TestClient(app)
 
 #Testa se retorna a lista
 def test_lista_jogos():
-    response = client.get("/jogos")
+    response = client.get("/Jogos")
     assert response.status_code == 200
 
 #Testa a quantidade de jogos na lista
 def test_quantidade_jogos():
-    response = client.get("/jogos")
+    response = client.get("/Jogos")
     data = response.json()
     assert len(data) == 3
 
 #Testa se contem o jogo específico
 def test_contem_thewitcher3():
-    response = client.get("/jogos")
+    response = client.get("/Jogos")
     data = response.json()
     nomes=[j["nome"] for j in data]
     assert "The Witcher 3 " in nomes
 
 #Testa a estrutura do objeto Jogo
 def test_estrutura_jogo():
-    response = client.get("/jogos")
+    response = client.get("/Jogos")
     data = response.json()
     assert "id" in data[0]
     assert "nome" in data[0]
